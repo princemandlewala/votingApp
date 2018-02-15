7728117f83b61537c3df9055ef32a0dbe334796e
Priyance <prince001mandlewala@gmail.com>
2018-02-14 21:15:15 -0500

    Updated read.php

--- items/read.php
+++ items/read.php
@@ -25,10 +25,7 @@ if($num>0){
     // products array
     $items_arr=array();
     $items_arr["records"]=array();
- 
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+  
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
         // extract row
         // this will make $row['name'] to
